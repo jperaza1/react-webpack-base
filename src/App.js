c7b2df0b88f8ac336b704ea8c314072a5514a013
jperaza1 <30508442+jperaza1@users.noreply.github.com>
2021-11-17 09:55:07 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './App.scss';
+
+export default function app() {
+  return (
+    <div className="app">
+      <h1>Hola Mundo</h1>
+      <p>React + Webpack + SASS</p>
+    </div>
+  )
+}
